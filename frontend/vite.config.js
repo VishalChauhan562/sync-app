92bf7697394e0b9d35fcfe4ac387ddad7e953174
Vishal <vishal-personal@MacBook-Pro.lan>
2025-01-22 00:53:24 +0530

    vite config updated for correct host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, 
+    port: 5173,  
+    hmr: {
+      host: 'sync-app-1.onrender.com',
+      clientPort: 5173,
+    },
+    allowedHosts: ['sync-app-1.onrender.com'],  
+  }
 })
