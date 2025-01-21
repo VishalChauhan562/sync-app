3c9a741fb5d63f27d12dd7a90f27e7c4ee2b1bec
Vishal <vishal-personal@MacBook-Pro.lan>
2025-01-21 22:19:56 +0530

    Deployment files ready

--- backend/app/services/firebase.py
+++ backend/app/services/firebase.py
@@ -1,9 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
+import json
 
 firsbase_creds = os.getenv("FIREBASE_CREDENTIALS_PATH")
-cred = credentials.Certificate(firsbase_creds)
+creds_dict = json.loads(firsbase_creds)
+cred = credentials.Certificate(creds_dict)
 firebase_admin.initialize_app(cred)
 
 print("Firebase initialized successfully")
